3de38f707e3434f2c1acedccf4504ddb14b81432
CankutBoraTuncer <77197110+CankutBoraTuncer@users.noreply.github.com>
2023-02-16 13:21:18 +0300

    Added the splash screen and editted the Sign Up page

--- Alicisindan/app/src/main/java/com/cankutboratuncer/alicisindan/SignUpActivity.java
+++ Alicisindan/app/src/main/java/com/cankutboratuncer/alicisindan/SignUpActivity.java
@@ -2,17 +2,13 @@ package com.cankutboratuncer.alicisindan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 
-public class MainActivity extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent splashIntent = new Intent(MainActivity.this, SplashActivity.class);
-        startActivity(splashIntent);
-
+        setContentView(R.layout.activity_sign_up);
     }
 }
