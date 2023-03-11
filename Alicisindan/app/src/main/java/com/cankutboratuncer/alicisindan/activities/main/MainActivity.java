a60f13d93c8e08836932cd100168cd4d80a7df98
CankutBoraTuncer <77197110+CankutBoraTuncer@users.noreply.github.com>
2023-03-11 16:02:26 +0300

    Added Menu

--- Alicisindan/app/src/main/java/com/cankutboratuncer/alicisindan/activities/main/MainActivity.java
+++ Alicisindan/app/src/main/java/com/cankutboratuncer/alicisindan/activities/main/MainActivity.java
@@ -1,20 +1,56 @@
 package com.cankutboratuncer.alicisindan.activities.main;
 
+
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
-import com.cankutboratuncer.alicisindan.databinding.ActivityMainBinding;
+import com.cankutboratuncer.alicisindan.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends AppCompatActivity {
-
-    ActivityMainBinding binding;
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        setContentView(R.layout.activity_main);
+        bottomNavigationView = findViewById(R.id.bottom_nav);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+        loadFragment(new HomeFragment());
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment = null;
+        switch (item.getItemId()) {
+            case R.id.menu_home:
+                fragment = new HomeFragment();
+                break;
+            case R.id.menu_buy:
+                fragment = new BuyFragment();
+                break;
+            case R.id.menu_sell:
+                fragment = new SellFragment();
+                break;
+            case R.id.menu_forum:
+                fragment = new ForumFragment();
+                break;
+            case R.id.menu_profile:
+                fragment = new ProfileFragment();
+                break;
+        }
+        if (fragment != null) {
+            loadFragment(fragment);
+        }
+        return true;
+    }
+
+    void loadFragment(Fragment fragment) {
+        //to attach fragment
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, fragment).commit();
     }
 }
-
