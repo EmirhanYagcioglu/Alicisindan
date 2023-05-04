c3dd25f8e5d6863f4409599285a982459380d205
CankutBoraTuncer <77197110+CankutBoraTuncer@users.noreply.github.com>
2023-05-04 18:37:37 +0300

    Adding Emirhan's work part 2

--- Alicisindan/app/src/main/java/com/cankutboratuncer/alicisindan/activities/ui/main/home/pages/BuyFragment.java
+++ Alicisindan/app/src/main/java/com/cankutboratuncer/alicisindan/activities/ui/main/home/pages/BuyFragment.java
@@ -4,45 +4,43 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.cankutboratuncer.alicisindan.R;
+import com.cankutboratuncer.alicisindan.activities.data.database.AdvertisementTest;
+import com.cankutboratuncer.alicisindan.activities.data.database.CategoryTest;
+import com.cankutboratuncer.alicisindan.activities.ui.main.advertisement.advertisement.AdvertisementAdapter;
+import com.cankutboratuncer.alicisindan.activities.ui.main.advertisement.advertisement.AdvertisementFragment;
+import com.cankutboratuncer.alicisindan.activities.ui.main.advertisement.advertisement.AdvertisementInterface;
+import com.cankutboratuncer.alicisindan.activities.ui.main.home.category.CategoryAdapter;
+import com.cankutboratuncer.alicisindan.activities.ui.main.home.category.CategoryFragment;
+import com.cankutboratuncer.alicisindan.activities.utilities.Advertisement;
+import com.cankutboratuncer.alicisindan.activities.utilities.Category;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link BuyFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class BuyFragment extends Fragment {
+public class BuyFragment extends Fragment implements AdvertisementInterface {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    ArrayList<Advertisement> advertisements;
+    ArrayList<Category> categories;
 
     public BuyFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment BuyFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static BuyFragment newInstance(String param1, String param2) {
+    public static BuyFragment newInstance(int advertisementID) {
         BuyFragment fragment = new BuyFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -51,14 +49,57 @@ public class BuyFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_buy, container, false);
+        View view = inflater.inflate(R.layout.fragment_buy, container, false);
+
+        LinearLayoutManager horizontalRecyclerViewLayoutManager = new LinearLayoutManager(view.getContext(), LinearLayoutManager.HORIZONTAL, false) {
+            @Override
+            public boolean checkLayoutParams(RecyclerView.LayoutParams lp) {
+                // force height of viewHolder here, this will override layout_height from xml
+                lp.width = (int) (getWidth() / 4);
+                return true;
+            }
+        };
+
+        RecyclerView recyclerViewForAdvertisements = view.findViewById(R.id.buyFragment_recyclerView_advertisements);
+        RecyclerView recyclerViewForCategories = view.findViewById(R.id.buyFragment_recyclerView_categories);
+
+        advertisements = AdvertisementTest.advertisements;
+        categories = CategoryTest.categories;
+
+        recyclerViewForAdvertisements.setLayoutManager(new GridLayoutManager(this.getContext(), 2));
+        recyclerViewForCategories.setLayoutManager(horizontalRecyclerViewLayoutManager);
+
+        AdvertisementAdapter advertisementAdapter = new AdvertisementAdapter(advertisements, this);
+        CategoryAdapter categoryAdapter = new CategoryAdapter(categories);
+        recyclerViewForAdvertisements.setAdapter(advertisementAdapter);
+        recyclerViewForCategories.setAdapter(categoryAdapter);
+
+        TextView textView_seeAll = view.findViewById(R.id.buyFragment_textView_seeAll);
+        textView_seeAll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Fragment fragment = new CategoryFragment();
+                loadFragment(fragment);
+            }
+        });
+
+        return view;
+    }
+
+    void loadFragment(Fragment fragment) {
+        //to attach fragment
+        getParentFragmentManager().beginTransaction().replace(R.id.mainActivity_frameLayout_main, fragment).addToBackStack(null).commit();
+    }
+
+    @Override
+    public void onItemClick(int position) {
+        Fragment fragment = AdvertisementFragment.newInstance(advertisements.get(position).getAdvertisementID());
+        loadFragment(fragment);
     }
 }
