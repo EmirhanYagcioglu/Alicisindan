1180e252c9669d5dc88a53e164f21c0e9dc9f4bd
CankutBoraTuncer <77197110+CankutBoraTuncer@users.noreply.github.com>
2023-05-18 14:37:42 +0300

    Threading the database retrival process and adding a loading screen

--- Alicisindan/app/src/main/java/com/cankutboratuncer/alicisindan/activities/ui/main/home/pages/HomeFragment.java
+++ Alicisindan/app/src/main/java/com/cankutboratuncer/alicisindan/activities/ui/main/home/pages/HomeFragment.java
@@ -1,10 +1,14 @@
 package com.cankutboratuncer.alicisindan.activities.ui.main.home.pages;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Base64;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,18 +22,15 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.cankutboratuncer.alicisindan.R;
-import com.cankutboratuncer.alicisindan.activities.data.database.AdvertisementTest;
 import com.cankutboratuncer.alicisindan.activities.data.database.CategoryTest;
-import com.cankutboratuncer.alicisindan.activities.ui.main.advertisement.advertisement.AdvertisementFragment;
 import com.cankutboratuncer.alicisindan.activities.ui.main.advertisement.advertisement.AdvertisementAdapter;
+import com.cankutboratuncer.alicisindan.activities.ui.main.advertisement.advertisement.AdvertisementFragment;
 import com.cankutboratuncer.alicisindan.activities.ui.main.advertisement.advertisement.AdvertisementInterface;
-import com.cankutboratuncer.alicisindan.activities.ui.main.advertisement.category.PostAddSubCategoryActivity;
-import com.cankutboratuncer.alicisindan.activities.utilities.Advertisement;
 import com.cankutboratuncer.alicisindan.activities.ui.main.advertisement.category.PostAddCategoryActivity;
-import com.cankutboratuncer.alicisindan.activities.utilities.AllCategories;
-import com.cankutboratuncer.alicisindan.activities.utilities.Category;
 import com.cankutboratuncer.alicisindan.activities.ui.main.home.category.CategoryAdapter;
 import com.cankutboratuncer.alicisindan.activities.ui.main.home.category.CategoryFragment;
+import com.cankutboratuncer.alicisindan.activities.utilities.Advertisement;
+import com.cankutboratuncer.alicisindan.activities.utilities.AllCategories;
 
 import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
@@ -40,6 +41,7 @@ import Alicisindan.User;
 public class HomeFragment extends Fragment implements AdvertisementInterface {
 
     ArrayList<Advertisement> advertisements;
+    View view;
     ArrayList<AllCategories> categories;
     String title;
     String description;
@@ -50,6 +52,10 @@ public class HomeFragment extends Fragment implements AdvertisementInterface {
     String userID;
     String username;
     String brand;
+    RecyclerView recyclerViewForAdvertisements;
+    RecyclerView recyclerViewForCategories;
+    Handler handler;
+    LinearLayoutManager horizontalRecyclerViewLayoutManager;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -72,13 +78,24 @@ public class HomeFragment extends Fragment implements AdvertisementInterface {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        advertisements = new ArrayList<>();
-        View view = inflater.inflate(R.layout.fragment_home, container, false);
-        view.findViewById(R.id.buttonCreatePost).setOnClickListener(v -> {
-            startActivity(new Intent(getContext(), PostAddCategoryActivity.class));
-        });
+        view = inflater.inflate(R.layout.fragment_home, container, false);
+        recyclerViewForAdvertisements = view.findViewById(R.id.homeFragment_recyclerView_advertisements);
+        recyclerViewForCategories = view.findViewById(R.id.homeFragment_recyclerView_categories);
+        initCategories();
+        initListeners();
+        return view;
+    }
 
-        LinearLayoutManager horizontalRecyclerViewLayoutManager = new LinearLayoutManager(view.getContext(), LinearLayoutManager.HORIZONTAL, false) {
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        loading(true, view);
+        handler = new Handler(Looper.getMainLooper());
+        new BackgroundTask(getContext(), this).execute();
+    }
+
+    public void initCategories(){
+        horizontalRecyclerViewLayoutManager = new LinearLayoutManager(view.getContext(), LinearLayoutManager.HORIZONTAL, false) {
             @Override
             public boolean checkLayoutParams(RecyclerView.LayoutParams lp) {
                 // force height of viewHolder here, this will override layout_height from xml
@@ -86,72 +103,16 @@ public class HomeFragment extends Fragment implements AdvertisementInterface {
                 return true;
             }
         };
-
-        RecyclerView recyclerViewForAdvertisements = view.findViewById(R.id.homeFragment_recyclerView_advertisements);
-        RecyclerView recyclerViewForCategories = view.findViewById(R.id.homeFragment_recyclerView_categories);
-
-        Listing[] listings;
-        try {
-            listings = Listing.findListings(null, null, null, null, null, null, null, null, null, null, null, "50");
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-        for ( int i = 0; i < listings.length; i++ )
-        {
-            Listing listing = listings[i];
-            title = listing.getTitle();
-            location = listing.getLocation();
-            description = listing.getDescription();
-            userID = listing.getOwnerID();
-            brand = listing.getBrand();
-
-            try {
-                User user = User.getUser(userID);
-                username = user.getUsername();
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-
-
-            try {
-                image = listing.getListingsFirstImage();
-                if(image == null){
-                    Bitmap icon = ((BitmapDrawable) ResourcesCompat.getDrawable(getResources(), R.drawable.img_baby, null)).getBitmap();
-                    image = encodeImage(icon);
-                }
-            } catch (Exception e) {
-            } finally {
-                if(image == null){
-                    Bitmap icon = ((BitmapDrawable) ResourcesCompat.getDrawable(getResources(), R.drawable.img_baby, null)).getBitmap();
-                    image = encodeImage(icon);
-                }
-            }
-
-            price = listing.getPrice();
-            ID = listing.getID();
-            advertisements.add( new Advertisement(title, description, image, price, ID, location, userID, username, brand));
-        }
-
         categories = CategoryTest.categories;
-
-        recyclerViewForAdvertisements.setLayoutManager(new GridLayoutManager(this.getContext(), 2));
-        recyclerViewForCategories.setLayoutManager(horizontalRecyclerViewLayoutManager);
-
-        AdvertisementAdapter advertisementAdapter = new AdvertisementAdapter(advertisements, this);
         CategoryAdapter categoryAdapter = new CategoryAdapter(categories);
-        recyclerViewForAdvertisements.setAdapter(advertisementAdapter);
         recyclerViewForCategories.setAdapter(categoryAdapter);
+        recyclerViewForCategories.setLayoutManager(horizontalRecyclerViewLayoutManager);
+    }
 
-        TextView textView_seeAll = view.findViewById(R.id.homeFragment_textView_seeAll);
-        textView_seeAll.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Fragment fragment = new CategoryFragment();
-                loadFragment(fragment);
-            }
+    public void initListeners(){
+        view.findViewById(R.id.buttonCreatePost).setOnClickListener(v -> {
+            startActivity(new Intent(getContext(), PostAddCategoryActivity.class));
         });
-
-        return view;
     }
 
     void loadFragment(Fragment fragment) {
@@ -177,6 +138,7 @@ public class HomeFragment extends Fragment implements AdvertisementInterface {
         loadFragment(fragment);
     }
 
+
     private String encodeImage(Bitmap bitmap) {
         int previewWidth = 150;
         int previewHeight = bitmap.getHeight() * previewWidth / bitmap.getWidth();
@@ -187,9 +149,166 @@ public class HomeFragment extends Fragment implements AdvertisementInterface {
         return Base64.encodeToString(bytes, Base64.DEFAULT);
     }
 
+    private class BackgroundTask extends AsyncTask<Void, Void, String> {
+        Context context;
+        AdvertisementInterface advertisementInterface;
+
+        public BackgroundTask(Context context, AdvertisementInterface advertisementInterface) {
+            this.context = context;
+            this.advertisementInterface = advertisementInterface;
+        }
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            // Simulate the database retrieval process
+            horizontalRecyclerViewLayoutManager = new LinearLayoutManager(view.getContext(), LinearLayoutManager.HORIZONTAL, false) {
+                @Override
+                public boolean checkLayoutParams(RecyclerView.LayoutParams lp) {
+                    // force height of viewHolder here, this will override layout_height from xml
+                    lp.width = (int) (getWidth() / 4);
+                    return true;
+                }
+            };
+            advertisements = new ArrayList<>();
+            // Retrieve data from the database
+            fetchDataFromDatabase();
+            categories = CategoryTest.categories;
+            return "Done";
+        }
+
+        @Override
+        protected void onPostExecute(final String data) {
+            Activity activity = getActivity();
+            if (activity != null) {
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        // Update the UI with the retrieved data
+                        AdvertisementAdapter advertisementAdapter = new AdvertisementAdapter(advertisements, advertisementInterface);
+                        CategoryAdapter categoryAdapter = new CategoryAdapter(categories);
+                        recyclerViewForAdvertisements.setAdapter(advertisementAdapter);
+                        recyclerViewForCategories.setAdapter(categoryAdapter);
+                        TextView textView_seeAll = view.findViewById(R.id.homeFragment_textView_seeAll);
+                        textView_seeAll.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                Fragment fragment = new CategoryFragment();
+                                loadFragment(fragment);
+                            }
+                        });
+                        ((Activity) context).runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                recyclerViewForAdvertisements.setLayoutManager(new GridLayoutManager(getContext(), 2));
+                                recyclerViewForCategories.setLayoutManager(horizontalRecyclerViewLayoutManager);
+                                loading(false, view);
+                            }
+                        });
+                    }
+                });
+            }
+        }
+
+        private void fetchDataFromDatabase() {
+            Listing[] listings;
+            try {
+                listings = Listing.findListings(null, null, null, null, null, null, null, null, null, null, null, "50");
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+            for (Listing listing : listings) {
+                title = listing.getTitle();
+                location = listing.getLocation();
+                description = listing.getDescription();
+                userID = listing.getOwnerID();
+                brand = listing.getBrand();
+
+                try {
+                    User user = User.getUser(userID);
+                    username = user.getUsername();
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+
+                try {
+                    image = listing.getListingsFirstImage();
+                    if (image == null) {
+                        Bitmap icon = ((BitmapDrawable) ResourcesCompat.getDrawable(getResources(), R.drawable.img_baby, null)).getBitmap();
+                        image = encodeImage(icon);
+                    }
+                } catch (Exception ignored) {
+                } finally {
+                    if (image == null) {
+                        Bitmap icon = ((BitmapDrawable) ResourcesCompat.getDrawable(getResources(), R.drawable.img_baby, null)).getBitmap();
+                        image = encodeImage(icon);
+                    }
+                }
+                price = listing.getPrice();
+                ID = listing.getID();
+                advertisements.add(new Advertisement(title, description, image, price, ID, location, userID, username, brand));
+            }
+        }
+    }
 
 
-//    public void createSearchBar(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public void findFromList(String searchedText, LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        try {
+            View view = inflater.inflate(R.layout.fragment_home, container, false);
+            RecyclerView recyclerViewForAdvertisements = view.findViewById(R.id.homeFragment_recyclerView_advertisements);
+            String text = searchedText;
+            Listing[] listings;
+            ArrayList<Advertisement> newAdvertisements = new ArrayList<Advertisement>();
+            String title;
+            String description;
+            String image;
+            String price;
+            String ID;
+            String location;
+            String userID;
+            String username;
+            String brand;
+            listings = Listing.findListings("", "", "", text, "", "", "", "", "", "", "", "10");
+            for (int i = 0; i < listings.length; i++) {
+                Listing listing = listings[i];
+                title = listing.getTitle();
+                description = listing.getDescription();
+                image = "0";
+                price = listing.getPrice();
+                location = listing.getLocation();
+                ID = listing.getID();
+                userID = listing.getOwnerID();
+                brand = listing.getBrand();
+                try {
+                    User user = User.getUser(userID);
+                    username = user.getUsername();
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+
+                newAdvertisements.add(new Advertisement(title, description, image, price, ID, location, userID, username, brand));
+            }
+            recyclerViewForAdvertisements.setLayoutManager(new GridLayoutManager(this.getContext(), 2));
+            AdvertisementAdapter advertisementAdapter = new AdvertisementAdapter(newAdvertisements, this);
+            recyclerViewForAdvertisements.setAdapter(advertisementAdapter);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void loading(boolean isLoading, View view) {
+        if (isLoading) {
+            view.findViewById(R.id.homeFragment_textView_featured).setVisibility(View.INVISIBLE);
+            view.findViewById(R.id.homeFragment_recyclerView_advertisements).setVisibility(View.INVISIBLE);
+            view.findViewById(R.id.progressBar).setVisibility(View.VISIBLE);
+        } else {
+            view.findViewById(R.id.homeFragment_textView_featured).setVisibility(View.VISIBLE);
+            view.findViewById(R.id.homeFragment_recyclerView_advertisements).setVisibility(View.VISIBLE);
+            view.findViewById(R.id.progressBar).setVisibility(View.INVISIBLE);
+        }
+    }
+
+    //    public void createSearchBar(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 //        View view = inflater.inflate(R.layout.fragment_home, container, false);
 //        RecyclerView recyclerViewForAdvertisements = view.findViewById(R.id.homeFragment_recyclerView_advertisements);
 //        searchView = view.findViewById(R.id.homeFragment_searchBar);
@@ -211,53 +330,5 @@ public class HomeFragment extends Fragment implements AdvertisementInterface {
 //            }
 //        });
 //    }
-
-
-    public void findFromList(String searchedText, LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        try {
-            View view = inflater.inflate(R.layout.fragment_home, container, false);
-            RecyclerView recyclerViewForAdvertisements = view.findViewById(R.id.homeFragment_recyclerView_advertisements);
-            String text = searchedText;
-            Listing[] listings;
-            ArrayList<Advertisement> newAdvertisements = new ArrayList<Advertisement>();
-            String title;
-            String description;
-            String image;
-            String price;
-            String ID;
-            String location;
-            String userID;
-            String username;
-            String brand;
-            listings = Listing.findListings("", "", "", text, "", "", "", "", "", "", "", "10");
-            for ( int i = 0; i < listings.length; i++ )
-            {
-                Listing listing = listings[i];
-                title = listing.getTitle();
-                description = listing.getDescription();
-                image = "0";
-                price = listing.getPrice();
-                location = listing.getLocation();
-                ID = listing.getID();
-                userID = listing.getOwnerID();
-                brand = listing.getBrand();
-                try {
-                    User user = User.getUser(userID);
-                    username = user.getUsername();
-                } catch (Exception e) {
-                    throw new RuntimeException(e);
-                }
-
-                newAdvertisements.add( new Advertisement(title, description, image, price, ID, location, userID, username, brand));
-            }
-            recyclerViewForAdvertisements.setLayoutManager(new GridLayoutManager(this.getContext(), 2));
-            AdvertisementAdapter advertisementAdapter = new AdvertisementAdapter(newAdvertisements, this);
-            recyclerViewForAdvertisements.setAdapter(advertisementAdapter);
-        } catch (Exception e)
-        {
-            e.printStackTrace();
-        }
-
-    }
 
 }
